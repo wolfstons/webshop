dd567c6016e50f08cbb688212e85fbcffad2d551
wolfstons <paczi.balazs@gmail.com>
2026-04-16 11:23:11 +0200

    adat bent

---
+++ js/adat.js
@@ -0,0 +1,62 @@
+const termekek = [
+  {
+    src: "img/termek1.jpg",
+    nev: "Okostelefon X1",
+    ar: 129990,
+    leiras: "Modern okostelefon nagy felbontású kijelzővel és gyors processzorral."
+  },
+  {
+    src: "img/termek2.jpg",
+    nev: "Laptop Pro 15",
+    ar: 349990,
+    leiras: "Erőteljes laptop munkához és játékhoz, hosszú akkumulátor idővel."
+  },
+  {
+    src: "img/termek3.jpg",
+    nev: "Vezeték nélküli fülhallgató",
+    ar: 24990,
+    leiras: "Kényelmes és kiváló hangzású bluetooth fülhallgató."
+  },
+  {
+    src: "img/termek4.jpg",
+    nev: "Okosóra Fit",
+    ar: 39990,
+    leiras: "Egészségfigyelő funkciókkal ellátott stílusos okosóra."
+  },
+  {
+    src: "img/termek5.jpg",
+    nev: "Tablet Plus",
+    ar: 89990,
+    leiras: "Könnyű és gyors tablet szórakozáshoz és munkához."
+  },
+  {
+    src: "img/termek6.jpg",
+    nev: "Bluetooth hangszóró",
+    ar: 19990,
+    leiras: "Hordozható hangszóró erőteljes basszussal."
+  },
+  {
+    src: "img/termek7.jpg",
+    nev: "Gaming egér",
+    ar: 14990,
+    leiras: "Precíz és gyors válaszidejű egér játékosoknak."
+  },
+  {
+    src: "img/termek8.jpg",
+    nev: "Mechanikus billentyűzet",
+    ar: 29990,
+    leiras: "Tartós és kényelmes billentyűzet RGB világítással."
+  },
+  {
+    src: "img/termek9.jpg",
+    nev: "Full HD monitor",
+    ar: 69990,
+    leiras: "Éles kép és széles látószögű kijelző irodai és otthoni használatra."
+  },
+  {
+    src: "img/termek10.jpg",
+    nev: "Külső SSD 1TB",
+    ar: 45990,
+    leiras: "Gyors adatátvitel és nagy tárhely hordozható kivitelben."
+  }
+];
